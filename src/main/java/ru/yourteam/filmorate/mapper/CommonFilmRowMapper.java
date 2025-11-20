1dd819c795fce5af7fcbd642213bce968879c31f
DokPlay <serrggeejjj@gmail.com>
2025-11-20 06:20:02 +0300

    **Align common liked films query and row mapper with aliased film ID/likes/release year to match DTO shape.**

--- src/main/java/ru/yourteam/filmorate/mapper/CommonFilmRowMapper.java
+++ src/main/java/ru/yourteam/filmorate/mapper/CommonFilmRowMapper.java
@@ -13,6 +13,8 @@ public class CommonFilmRowMapper implements RowMapper<CommonFilmDto> {
         CommonFilmDto commonFilmDto = new CommonFilmDto();
         commonFilmDto.setId(resultSet.getLong("id"));
         commonFilmDto.setName(resultSet.getString("name"));
+        commonFilmDto.setLikes(resultSet.getInt("likes"));
+        commonFilmDto.setReleaseYear(resultSet.getInt("release_year"));
         return commonFilmDto;
     }
 }
--- src/main/java/ru/yourteam/filmorate/repository/CommonFilmRepository.java
+++ src/main/java/ru/yourteam/filmorate/repository/CommonFilmRepository.java
@@ -12,15 +12,19 @@ import java.util.List;
 @Service
 public class CommonFilmRepository extends BaseRepository<CommonFilmDto> {
 
-    private static final String FIND_COMMON_FILMS = "SELECT f.* " +
+    private static final String FIND_COMMON_FILMS = "SELECT " +
+                                                    "    f.film_id AS id, " +
+                                                    "    f.film_name AS name, " +
+                                                    "    COALESCE(COUNT(l.user_id), 0) AS likes, " +
+                                                    "    EXTRACT(YEAR FROM f.release_date) AS release_year " +
                                                     "FROM likes la " +
                                                     "JOIN likes lb ON la.film_id = lb.film_id " +
-                                                    "AND la.user_id = ? " +
-                                                    "AND lb.user_id = ? " +
+                                                    "    AND la.user_id = ? " +
+                                                    "    AND lb.user_id = ? " +
                                                     "JOIN films f  ON f.film_id = la.film_id " +
                                                     "LEFT JOIN likes l ON l.film_id = f.film_id " +
-                                                    "GROUP BY f.film_id " +
-                                                    "ORDER BY COUNT(l.user_id) DESC, f.film_id ASC " +
+                                                    "GROUP BY f.film_id, f.film_name, f.release_date " +
+                                                    "ORDER BY likes DESC, f.film_id ASC " +
                                                     "LIMIT ? OFFSET ?";
 
     public CommonFilmRepository(JdbcTemplate jdbc, CommonFilmRowMapper mapper) {
