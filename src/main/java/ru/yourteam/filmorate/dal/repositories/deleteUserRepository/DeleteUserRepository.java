d098c6fa30a15bbe19560c0fff9452b87e1c2f17
DokPlay <serrggeejjj@gmail.com>
2025-11-24 03:54:19 +0300

    fix 10.0

--- src/main/java/ru/yourteam/filmorate/dal/repositories/deleteUserRepository/DeleteUserRepository.java
+++ src/main/java/ru/yourteam/filmorate/dal/repositories/deleteUserRepository/DeleteUserRepository.java
@@ -3,22 +3,19 @@ package ru.yourteam.filmorate.dal.repositories.deleteUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ru.yourteam.filmorate.repository.UserRepository;
 
 @Repository
 @RequiredArgsConstructor
 public class DeleteUserRepository {
 
-    // JdbcTemplate и репозиторий объявлены как финальные поля для надежной инъекции зависимостей
+    // JdbcTemplate объявлен как финальное поле для надежной инъекции зависимости
     private final JdbcTemplate jdbc;
-    private final UserRepository userRepository;
 
     // Подготовленный запрос, который принимает идентификатор пользователя как параметр
     private static final String DELETE_FROM_USERS_QUERY = "DELETE FROM users WHERE user_id = ?";
 
 
     public void deleteUserById(int userId) {
-        userRepository.ensureUserExists(userId);
         // Поскольку во всех таблицах ON DELETE CASCADE, удаляем только из users
         // Передаем идентификатор как параметр в подготовленный запрос, чтобы исключить SQL-инъекции
         jdbc.update(DELETE_FROM_USERS_QUERY, userId);
